5499edf66968fd026a83e3783a5f970ef541e430
ellie <ririhan217@gmail.com>
2022-02-19 16:45:26 +0900

    refactor(racingcar): 랜덤 시작 값 추가

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -4,12 +4,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomNumberGenerator {
 
+    private static final int START_EXCLUSIVE = 0;
     private static final int END_EXCLUSIVE = 10;
 
     private RandomNumberGenerator() {
     }
 
     public static int pickRandomNumber() {
-        return ThreadLocalRandom.current().nextInt(END_EXCLUSIVE);
+        return ThreadLocalRandom.current().nextInt(START_EXCLUSIVE, END_EXCLUSIVE);
     }
 }
